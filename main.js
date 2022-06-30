7f3072809ca1d00b20deb49e0a609b8b3e31f853
anushka3105 <93994734+anushka3105@users.noreply.github.com>
2022-06-30 19:43:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0
+noseY=0
+
 function preload() {
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 
@@ -21,6 +25,7 @@ function modelloaded() {
 
 function draw() {
     image(video , 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 30, 30);
 }
 
 
@@ -33,8 +38,10 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x)
-        console.log("nose y = " + results[0].pose.nose.y)
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
